631eebc2b76373309054fec4a2e7595546786e18
Gabriel Berserk <143458446+Gabsjormungand@users.noreply.github.com>
2023-11-13 23:28:11 +0000

    laço faça enquanto

---
+++ Aula Não na sala de origem/atividade2.js
@@ -0,0 +1,15 @@
+// Laço Faça equanto
+
+let i = 1
+do{
+    console.log("Botafogo Campeão")
+    i++
+}while (i<1);
+
+let contador = 0
+do{
+    console.log(contador)
+    contador++
+}while(contador<10){
+ cconsole.log("===>" = contador)
+}
