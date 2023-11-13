7d4aa438b700f4f82c3a10e4df6dc9d49a1bae24
Gabriel Berserk <143458446+Gabsjormungand@users.noreply.github.com>
2023-11-13 23:20:53 +0000

    laço enquanto-while

---
+++ Aula Não na sala de origem/teste.js
@@ -0,0 +1,9 @@
+let idade = 15
+
+if(idade = 15 ){
+    console.log("O Voto é opcional")
+}else if (idade > 15){
+    console.log("Você não pode votar ❌")
+}else if(idade <= 18){
+    console.log("Você é obrigado a votar")
+}
