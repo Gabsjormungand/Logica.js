a7e45b8793030abc3c6f649688fcf5dc6fb3a985
Gabriel Berserk <143458446+Gabsjormungand@users.noreply.github.com>
2023-11-14 22:42:41 +0000

    exercicio 4

---
+++ Aula Não na sala de origem/exer3.js
@@ -0,0 +1,8 @@
+// 3. Faça um programa que receba dois números inteiros e gere os números inteiros que estão no intervalo compreendido por eles.
+let inicio = 1
+let fim = 50
+
+while(inicio < fim-1){
+    inicio++
+    console.log(inicio)
+}
