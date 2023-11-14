3f0a21da54dc91f8168f21af8972d94fb91981fc
Gabriel Berserk <143458446+Gabsjormungand@users.noreply.github.com>
2023-11-14 22:57:18 +0000

    exercicio 4

--- Aula Não na sala de origem/exer4.js
+++ Aula Não na sala de origem/exer4.js
@@ -1,1 +1,11 @@
 // 4. Altere o programa anterior para mostrar no final a soma dos números.
+let inicio = 1
+let fim = 50
+let soma = 0
+
+while(inicio < fim-1){
+    inicio++
+    console.log(inicio)
+    soma += inicio
+}
+console.log(soma)
